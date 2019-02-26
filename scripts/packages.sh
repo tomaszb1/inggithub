92a02d459fdb4fcf40ac4bd03b15d8a2924d880d
tomasz1 <tomasz.boch@gmail.com>
2019-02-26 17:30:18 +0100

    commit

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -7,10 +7,6 @@ set -euf -o pipefail
 ############################################################
 # Taken from here: https://gist.github.com/kwk/55bb5b6a4b7457bef38d
 #
-# this forces dpkg not to call sync() after package extraction and speeds up
-# install
-echo "force-unsafe-io" > /etc/dpkg/dpkg.cfg.d/02apt-speedup
-#
 # we don't need and apt cache in a container
 echo "Acquire::http {No-Cache=True;};" > /etc/apt/apt.conf.d/no-cache
 
